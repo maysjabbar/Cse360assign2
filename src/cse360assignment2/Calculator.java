1d83752e70c2ac61a0d3ae2e2421e0832d7ea037
maysjabbar5 <maysjabbar5@gmail.com>
2019-02-25 14:18:20 -0700

    added comments to calculator.java

---
+++ src/cse360assignment2/Calculator.java
@@ -0,0 +1,73 @@
+/*
+     * @author: Mays Jabbar
+     * Class ID: 415
+     * Assignment #: 2
+     * 
+     * 
+     * This class contains a simple calculator subtraction, addition, multiplication, and dividing
+     * The subtract method should subtract the parameter.
+     * The get total method should return the total.
+     * the multiply method shpuld multiply the total * parameters.
+     * 
+     * 
+     */
+package cse360assignment2;
+/** 
+ * This class contains calculation that has some methods.
+ * adding the method, subtracting method, get the total and return it
+ * multiply method that should multiply the total by the parameter
+ * the divide method should divide the total by the parameter and also
+ * a getHistory method.
+ * 
+ * @author:     Mays Jabbar
+ */
+public class Calculator {
+
+  private int total;
+	 /**
+         * This is a constructor method that initializes the total to 0. 
+         */
+	public Calculator () {
+		total = 0;  
+	}
+	/**
+         * this is a method that returns the current value of the total.
+         * @return 
+         */
+	public int getTotal () {
+		return 0;
+	}
+	/**
+         * This is an add method that should add the parameter to the total variable. 
+         */
+	public void add (int value) {
+		
+	}
+		/**
+         * This is a method that subtract the parameter from the total variable.
+         */
+
+	public void subtract (int value) {
+		
+	}
+	/**
+         * This is a multiply method the total by the parameter.
+         */
+	public void multiply (int value) {
+		
+	}
+	/**
+         * The divide method is dividing the total by the parameter by using an integer division
+         * If the parameter is 0 then set the total to 0.
+         */
+	public void divide (int value) {
+		
+	}
+	/**
+         * The get history method will use normal operation, which returns the history of all the actions as a string.
+         * @return a String that separates all operations and values with a space.
+         */
+	public String getHistory () {
+		return "";
+	}
+}
